e41f437ecf9b5163e1d57b80a877d1f224e610c3
Thomas Williams <thomas.williams@live.co.uk>
2016-09-22 11:20:06 +0200

    Starting new project - lecture 08.

---
+++ workspace/08_UserInput/src/08_UserInput.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : 08_UserInput.cpp
+// Author      : Thomas Williams
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
