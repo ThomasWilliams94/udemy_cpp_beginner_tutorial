f83b47ca6bff963cb3e0d794b11fe54710117d5b
Thomas Williams <thomas.williams@live.co.uk>
2016-09-22 11:56:03 +0200

    Writing out code for lecture 10 myself.

--- workspace/10_IntegerTypes/src/10_IntegerTypes.cpp
+++ workspace/10_IntegerTypes/src/10_IntegerTypes.cpp
@@ -7,9 +7,32 @@
 //============================================================================
 
 #include <iostream>
+#include <climits>
 using namespace std;
 
 int main() {
+
+	int value = -54645;
+	cout << value << endl;
+
+	cout << "Max int value: " << INT_MAX << endl;
+	cout << "Min int value: " << INT_MIN << endl;
+	cout << "Long max value: " << LONG_MAX << endl;
+
+	long long int lValue = 234543453453;
+	cout << lValue << endl;
+
+	short int sValue = 23434;
+	cout << sValue << endl;
+
+	cout << "Size of int: " << sizeof(int) << endl;
+	cout << "Size of long int: " << sizeof(long int) << endl;
+	cout << "Size of long long int: " << sizeof(long long int) << endl;
+	cout << "Size of short int: " << sizeof(short int) << endl;
+
+	unsigned int uValue = 25468;
+	cout << uValue << endl;
+	cout << "Size of unsigned int: " << sizeof(unsigned int) << endl;
 
 	return 0;
 }
