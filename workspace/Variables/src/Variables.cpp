a89cdd3b40e316d8c05b246a7aaace365780c455
Thomas Williams <thomas.williams@live.co.uk>
2016-09-22 10:54:31 +0200

    Typing out code for lecture 06 myself.

--- workspace/Variables/src/Variables.cpp
+++ workspace/Variables/src/Variables.cpp
@@ -10,6 +10,27 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	int numberCats = 5;
+	int numberDogs = 7;
+	int numberAnimals = numberCats + numberDogs;
+
+	cout << "Number of cats: " << numberCats << endl;
+	cout << "Number of dogs: " << numberDogs << endl;
+
+	cout << "Total number of animals: " << numberAnimals << endl;
+
+	cout << "New dog acquired!" << endl;
+
+	// Increment the number of dogs by 1. (But ONLY numberDogs, not numberAnimals)
+	++numberDogs;
+
+	cout << "New number of dogs: " << numberDogs << endl;
+
+	// Must also "recalculate" numberAnimals
+	numberAnimals = numberCats + numberDogs;
+
+	cout << "New total number of animals: " << numberAnimals << endl;
+
 	return 0;
 }
