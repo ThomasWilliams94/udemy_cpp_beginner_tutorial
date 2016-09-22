12ec6cd02096bb1d660bf4c024f47115ec67c1cc
Thomas Williams <thomas.williams@live.co.uk>
2016-09-22 10:35:45 +0200

    Typing out program myself.

--- workspace/OutputtingText/src/OutputtingText.cpp
+++ workspace/OutputtingText/src/OutputtingText.cpp
@@ -1,15 +1,19 @@
-//============================================================================
-// Name        : OutputtingText.cpp
-// Author      : Thomas Williams
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-#include <iostream>
+			#include <iostream>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	// flush does not create a new line
+cout << "Starting program..." << flush;
+
+	// endl does create a new line
+	cout << "This is some text." << endl;
+
+	// This second line outputted on new line
+	cout << "This is a second line of text." << endl;
+
+	// Outputting more than one string in one statement
+	// Also demonstrates different ways of including a space.
+	cout << "Banana." << " " << "Apple. " << "Orange." << endl;
 	return 0;
 }
