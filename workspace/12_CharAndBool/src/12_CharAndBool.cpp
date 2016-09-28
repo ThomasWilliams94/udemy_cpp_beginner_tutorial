bf7846abc41e10041e4f0a46cdc9829e7a69a75c
Thomas Williams <thomas.williams@live.co.uk>
2016-09-28 10:27:29 +0200

    Writing out code for lecture 12 myself.

--- workspace/12_CharAndBool/src/12_CharAndBool.cpp
+++ workspace/12_CharAndBool/src/12_CharAndBool.cpp
@@ -11,6 +11,21 @@ using namespace std;
 
 int main() {
 
+	bool bValue = true;
+	cout << bValue << endl;
+
+	char cValue1 = '55';
+	char cValue2 = 55;
+
+	cout << cValue1 << endl;
+	cout << (int)cValue1 << endl;
+	cout << cValue2 << endl;
+	cout << "Size of char: " << sizeof(char) << endl;
+
+	wchar_t wValue = 'i';
+
+	cout << (char)wValue << endl;
+	cout << "Size of wchar_t: " << sizeof(wchar_t) << endl;
 
 	return 0;
 }
