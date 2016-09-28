38ec9ba61ddacf2994bd556a7b9a2f948d498c93
Thomas Williams <thomas.williams@live.co.uk>
2016-09-28 10:17:34 +0200

    Writing out code for lecture 11 myself.

--- workspace/11_FloatingPointTypes/src/11_FloatingPointTypes.cpp
+++ workspace/11_FloatingPointTypes/src/11_FloatingPointTypes.cpp
@@ -7,9 +7,37 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	float fValue = 123.456789876543210;
+
+	cout << sizeof(float) << endl;
+	cout << sizeof(fValue) << endl;
+
+	// To use "fixed" and "setprecision", must #include <iomanip>
+	cout << fixed << fValue << endl;
+	cout << setprecision(40) << fValue << endl;
+
+    double dValue = 123.456789876543210;
+
+	cout << sizeof(double) << endl;
+	cout << sizeof(dValue) << endl;
+
+	cout << dValue << endl;
+
+
+	long double lValue = 123.456789876543210123456;
+
+	cout << sizeof(lValue) << endl;
+
+	cout << lValue << endl;
+
+	// It appears that the double type is automatically long
+	// with this compiler... but not sure
+
 	return 0;
 }
