cc637499f06720487f2887eef91372365a736955
Thomas Williams <thomas.williams@live.co.uk>
2016-09-28 10:57:01 +0200

    Writing code for lecture 14 myself.

--- workspace/14_IfElse/src/14_IfElse.cpp
+++ workspace/14_IfElse/src/14_IfElse.cpp
@@ -11,5 +11,24 @@ using namespace std;
 
 int main() {
 
+	cout << "1.\tAdd new record." << endl;
+	cout << "2.\tDelete record." << endl;
+	cout << "3.\tView record." << endl;
+	cout << "4.\tSearch record." << endl;
+	cout << "5.\tQuit." << endl;
+
+	cout << "Enter your selection > " << flush;
+
+	int value;
+
+	cin >> value;
+
+	if(value < 3) {
+		cout << "Insufficient privileges to use this menu option." << endl;
+	}
+	else {
+		cout << "Privilege level sufficient." << endl;
+	}
+
 	return 0;
 }
