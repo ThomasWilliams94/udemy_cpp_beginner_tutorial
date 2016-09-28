25e673ed5bb4e5f27b73c7bb83f8f0ce915dd9d5
Thomas Williams <thomas.williams@live.co.uk>
2016-09-28 11:48:35 +0200

    Writing code for lecture 16 myself.

--- workspace/16_ComparingFloats/src/16_ComparingFloats.cpp
+++ workspace/16_ComparingFloats/src/16_ComparingFloats.cpp
@@ -7,10 +7,22 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 
+	float value1 = 1.1;
+
+	if(value1 == 1.1) {
+		cout << "equals" << endl;
+	} else {
+		cout << "not equal" << endl;
+	}
+
+	cout << setprecision(10) << endl;
+
+	cout << value1 << endl;
 
 	return 0;
 }
