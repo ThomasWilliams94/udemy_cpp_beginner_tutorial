60f766abf1eb3ba310a3c817a5b9d220fe820760
Thomas Williams <thomas.williams@live.co.uk>
2016-10-05 10:54:25 +0200

    Writing code for lecture 21 myself.

--- workspace/21_BreakAndContinue/src/21_BreakAndContinue.cpp
+++ workspace/21_BreakAndContinue/src/21_BreakAndContinue.cpp
@@ -10,6 +10,49 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	// break ...
+	for (int i = 0; i < 5; ++i) {
+		cout << "i is: " << i << endl;
+
+		if (i == 3) {
+			break;
+		}
+
+		cout << "Looping" << endl;
+	}
+
+	// continue ...
+	for (int i = 0; i < 5; ++i) {
+		cout << "i is: " << i << endl;
+
+		if (i == 3) {
+			continue;
+		}
+
+		cout << "Looping" << endl;
+	}
+
+	const string password = "hello"; // cannot reassign value of password with const
+
+	string input;
+
+	do {
+		cout << "Enter your password > " << flush;
+
+		cin >> input;
+
+		if (input == password) {
+			break;
+		} else {
+			cout << "Access denied." << endl;
+		}
+
+	} while (true);
+
+	cout << "Password accepted." << endl;
+
+	cout << "Program quitting." << endl;
+
 	return 0;
 }
