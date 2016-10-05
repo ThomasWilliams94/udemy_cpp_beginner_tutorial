8928d51961a443c76339e18264995769d0f5402c
Thomas Williams <thomas.williams@live.co.uk>
2016-10-05 13:54:51 +0200

    Starting new project - lecture 26.

---
+++ workspace/26_Switch/src/26_Switch.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : 26_Switch.cpp
+// Author      : Thomas Williams
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
