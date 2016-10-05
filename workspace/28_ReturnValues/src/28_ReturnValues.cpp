4d8e0689242aeac481f8602d765d1a9031b3feba
Thomas Williams <thomas.williams@live.co.uk>
2016-10-05 14:21:42 +0200

    Starting new project - lecture 28.

---
+++ workspace/28_ReturnValues/src/28_ReturnValues.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : 28_ReturnValues.cpp
+// Author      : Thomas Williams
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
