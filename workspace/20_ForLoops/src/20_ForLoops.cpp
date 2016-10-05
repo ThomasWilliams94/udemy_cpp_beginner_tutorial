382df8abc0001eacdbe078fc20db8baca36bb088
Thomas Williams <thomas.williams@live.co.uk>
2016-10-05 10:45:45 +0200

    Writing code for lecture 20 myself.

--- workspace/20_ForLoops/src/20_ForLoops.cpp
+++ workspace/20_ForLoops/src/20_ForLoops.cpp
@@ -10,6 +10,10 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	for (int i = 0; i < 10; ++i) {
+		cout << "Hello " << i << endl;
+	}
+
 	return 0;
 }
