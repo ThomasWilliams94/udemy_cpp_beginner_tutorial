e37daf9a575e6e3784dcbde12a8a3b9590743786
Thomas Williams <thomas.williams@live.co.uk>
2016-10-05 14:09:48 +0200

    Writing code for lecture 26 myself.

--- workspace/26_Switch/src/26_Switch.cpp
+++ workspace/26_Switch/src/26_Switch.cpp
@@ -11,7 +11,21 @@ using namespace std;
 
 int main() {
 
+	int value = 18;
 
+	switch (value) {
+	case 4:
+		cout << "Value is 4." << endl;
+		break;
+	case 5:
+		cout << "Value is 5." << endl;
+		break;
+	case 6:
+		cout << "Value is 6." << endl;
+		break;
+	default:
+		cout << "Unrecognised value." << endl;
+	}
 
 	return 0;
 }
