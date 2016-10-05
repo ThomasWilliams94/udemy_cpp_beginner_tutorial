c5ab72ef4b9dff9839086c2dc843c6177ca4cd11
Thomas Williams <thomas.williams@live.co.uk>
2016-10-05 13:48:54 +0200

    Writing code for lecture 24 myself.

--- workspace/24_Sizeof/src/24_Sizeof.cpp
+++ workspace/24_Sizeof/src/24_Sizeof.cpp
@@ -11,7 +11,16 @@ using namespace std;
 
 int main() {
 
+	int values[] = {4, 3, 7, 8};
 
+	cout << sizeof(values) << endl;
+	cout << sizeof(int) << endl;
+
+	for(unsigned int i=0; i<sizeof(values)/sizeof(int); i++) {
+		cout << values[i] << " " << flush;
+	}
+
+	cout << endl;
 
 	return 0;
 }
