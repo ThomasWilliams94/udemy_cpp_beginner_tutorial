fae570b5d0425d9a8feb82b03812e7e841132489
Thomas Williams <thomas.williams@live.co.uk>
2016-10-12 17:05:41 +0200

    Writing code for lecture 39 for myself.

--- workspace/39_Pointers/src/39_Pointers.cpp
+++ workspace/39_Pointers/src/39_Pointers.cpp
@@ -9,8 +9,38 @@
 #include <iostream>
 using namespace std;
 
+// Purpose of function was changed throughout tutorial,
+// and I couldn't be bothered to commit different versions
+// and explain what was going on >_<
+void manipulate(double* value) {
+	*value = 10.2;
+	cout << "Value of double in manipulate: " << *value << endl;
+}
+
 int main() {
 
+	int nValue = 8;
+
+	// can also write:
+	// int *pnValue = &nValue;
+	int* pnValue = &nValue;
+
+	cout << "Int value: " << nValue << endl;
+	cout << "Pointer to int value: " << pnValue << endl << endl;
+
+	nValue = 9;
+
+	cout << "Int value: " << nValue << endl;
+	cout << "Pointer to int value: " << pnValue << endl;
+	cout << "Int value via pointer: " << *pnValue << endl;
+
+	cout << endl << "===================" << endl;
+
+	double dValue = 1.234;
+
+	cout << "1. dValue: " << dValue << endl;
+	manipulate(&dValue);
+	cout << "2. dValue: " << dValue << endl;
 
 	return 0;
 }
