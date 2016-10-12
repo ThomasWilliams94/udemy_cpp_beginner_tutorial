a5f1357bebee206bb13f3b3951615e0812b45e4d
Thomas Williams <thomas.williams@live.co.uk>
2016-10-12 10:16:40 +0200

    Writing code for lecture 35 myself.

--- workspace/35_StringStreams/src/35_StringStreams.cpp
+++ workspace/35_StringStreams/src/35_StringStreams.cpp
@@ -7,11 +7,26 @@
 //============================================================================
 
 #include <iostream>
+#include <sstream>
+
 using namespace std;
 
 int main() {
 
+	string name = "Bob";
+	int age = 32;
 
+	stringstream ss;
+
+	ss << "Name is: ";
+	ss << name;
+	ss << "; Age is: ";
+	ss << age;
+
+	string info = ss.str();
+
+	cout << ss.str() << endl;
+	cout << info << endl;
 
 	return 0;
 }
