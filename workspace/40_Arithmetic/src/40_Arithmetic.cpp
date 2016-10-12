ee34220f177acaeabd036583b1f0417197101a93
Thomas Williams <thomas.williams@live.co.uk>
2016-10-12 17:15:19 +0200

    Writing out code for lecture 40 myself.

--- workspace/40_Arithmetic/src/40_Arithmetic.cpp
+++ workspace/40_Arithmetic/src/40_Arithmetic.cpp
@@ -11,5 +11,26 @@ using namespace std;
 
 int main() {
 
+	double value1 = (double)7/2;
+	cout << value1 << endl;
+
+	int value2 = (int)7.3;
+	cout << value2 << endl;
+
+	int value3 = 8;
+	value3 += 1; // value3 = value3 + 1 OR value3++ OR ++value3;
+	cout << value3 << endl;
+
+	int value4 = 10;
+	value4 /= 5;
+	cout << value4 << endl;
+
+	int value5 = 17%3;
+	cout << value5 << endl;
+
+	int value6 = 17;
+	value6 %= 3;
+	cout << value6 << endl;
+
 	return 0;
 }
