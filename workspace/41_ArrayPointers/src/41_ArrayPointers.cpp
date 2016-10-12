96fb76f80673d443cbce5868d56fc9caef013c68
Thomas Williams <thomas.williams@live.co.uk>
2016-10-12 17:28:53 +0200

    Writing code for lecture 41 myself.

--- workspace/41_ArrayPointers/src/41_ArrayPointers.cpp
+++ workspace/41_ArrayPointers/src/41_ArrayPointers.cpp
@@ -11,6 +11,40 @@ using namespace std;
 
 int main() {
 
+	string texts[] = { "one", "two", "three" };
+
+	string* pTexts = texts;
+
+	for (unsigned int i = 0; i < sizeof(texts) / sizeof(string); ++i) {
+		cout << pTexts[i] << " " << flush;
+	}
+
+	cout << endl;
+
+	for (unsigned int i = 0; i < sizeof(texts) / sizeof(string); ++i, ++pTexts) {
+		cout << *pTexts << " " << flush;
+	}
+
+	string* pElement = texts;
+	string* pEnd = &(texts[2]); // getting last element in array
+
+	cout << endl;
+
+	while(true) {
+		cout << *pElement << " " << flush;
+
+		if(pElement == pEnd) {
+			break;
+		}
+
+
+		++pElement;
+
+	}
+
+
+
+
 
 	return 0;
 }
