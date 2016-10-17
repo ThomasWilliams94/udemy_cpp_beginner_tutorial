572932a696df443a530ad08a4125a2a3a79b3dd0
Thomas Williams <thomas.williams@live.co.uk>
2016-10-17 18:00:00 +0200

    Writing code for lecture 49 myself.

--- workspace/49_ReturningObjectsFromFunctions/src/49_ReturningObjectsFromFunctions.cpp
+++ workspace/49_ReturningObjectsFromFunctions/src/49_ReturningObjectsFromFunctions.cpp
@@ -9,7 +9,54 @@
 #include <iostream>
 using namespace std;
 
+class Animal {
+private:
+	string name;
+
+public:
+	Animal() {
+		cout << "Animal created." << endl;
+	}
+	;
+	Animal(const Animal& other) :
+			name(other.name) {
+		cout << "Animal created by copying" << endl;
+	}
+	;
+
+	~Animal() {
+		cout << "Destructor called" << endl;
+	}
+	;
+
+	void setName(string name) {
+		this->name = name;
+	}
+	;
+	void speak() const {
+		cout << "My name is: " << name << endl;
+	}
+	;
+};
+
+Animal *createAnimal() {
+	Animal *pAnimal = new Animal();
+	pAnimal->setName("Berty");
+	return pAnimal;
+}
+
 int main() {
 
+	Animal *pCat1 = new Animal();
+	(*pCat1).setName("Freddy");
+	pCat1->speak();
+	delete pCat1;
+
+	cout << endl;
+
+	Animal *pFrog = createAnimal();
+	pFrog->speak();
+
+	delete pFrog;
 	return 0;
 }
