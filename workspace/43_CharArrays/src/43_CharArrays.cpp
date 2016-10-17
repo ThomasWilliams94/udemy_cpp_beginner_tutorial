662f68448b5bf4057d2452a32e716a9bef967475
Thomas Williams <thomas.williams@live.co.uk>
2016-10-17 15:32:35 +0200

    Writing code for lecture 43 myself.

--- workspace/43_CharArrays/src/43_CharArrays.cpp
+++ workspace/43_CharArrays/src/43_CharArrays.cpp
@@ -11,6 +11,31 @@ using namespace std;
 
 int main() {
 
+	string text = "hello";
+
+	cout << text << endl;
+
+	char textArray[] = "hello";
+
+	cout << textArray << endl;
+
+	// note the null string character
+	for (int i = 0; i < sizeof(textArray); i++) {
+		cout << i << ": " << textArray[i] << endl;
+	}
+
+	int k = 0;
+
+	while (true) {
+
+		if (text[k] == 0) {
+			break;
+		}
+
+		cout << text[k] << flush;
+
+		k++;
+	}
 
 	return 0;
 }
