fd9c007b88da75086f87605940b014d18309217e
Thomas Williams <thomas.williams@live.co.uk>
2016-10-17 17:45:30 +0200

    Writing out code for lecture 48 myself.

--- workspace/48_NewOperator/src/48_NewOperator.cpp
+++ workspace/48_NewOperator/src/48_NewOperator.cpp
@@ -9,7 +9,44 @@
 #include <iostream>
 using namespace std;
 
+class Animal {
+private:
+	string name;
+
+public:
+	Animal() {
+		cout << "Animal created." << endl;
+	}
+	;
+	Animal(const Animal& other) :
+			name(other.name) {
+		cout << "Animal created by copying" << endl;
+	}
+	;
+
+	~Animal() {
+		cout << "Destructor called" << endl;
+	}
+	;
+
+	void setName(string name) {
+		this->name = name;
+	}
+	;
+	void speak() const {
+		cout << "My name is: " << name << endl;
+	}
+	;
+};
+
 int main() {
+
+	Animal *pCat1 = new Animal(); // when memory is allocated with "new", you MUST destroy the object manually
+
+	(*pCat1).setName("Freddy"); // possible, but ugly syntax
+	pCat1->speak(); // better syntax.
+
+	delete pCat1; // always call delete if you call new!
 
 	return 0;
 }
