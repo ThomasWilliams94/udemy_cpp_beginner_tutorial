336b6353b0f6ada162ccccfa6b5e39187003631c
Thomas Williams <thomas.williams@live.co.uk>
2016-10-17 15:45:32 +0200

    Starting new project - lecture 45.

---
+++ workspace/45_References/src/45_References.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : 45_References.cpp
+// Author      : Thomas Williams
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
