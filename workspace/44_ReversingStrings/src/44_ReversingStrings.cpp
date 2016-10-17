81e0f6212d710494ede1ae7a4c960002a1b55e4a
Thomas Williams <thomas.williams@live.co.uk>
2016-10-17 15:44:51 +0200

    Writing code for lecture 44 myself.

--- workspace/44_ReversingStrings/src/44_ReversingStrings.cpp
+++ workspace/44_ReversingStrings/src/44_ReversingStrings.cpp
@@ -11,5 +11,26 @@ using namespace std;
 
 int main() {
 
+	char text[] = "palindrome";
+
+	int nChars = sizeof(text) - 1; // -1 because of null character at end of string
+
+	// declare pointer to start of array text
+	char *pStart = text;
+
+	// declare point to end of array text
+	char *pEnd = text + nChars - 1;
+
+	while(pStart < pEnd) {
+
+		char tempStart = *pStart;
+		*pStart = *pEnd;
+		*pEnd = tempStart;
+		++pStart;
+		--pEnd;
+	}
+
+	cout << text << endl;
+
 	return 0;
 }
