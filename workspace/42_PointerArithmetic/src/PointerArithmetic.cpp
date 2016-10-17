c55d96e492175958632ead876cbb16d0552e8735
Thomas Williams <thomas.williams@live.co.uk>
2016-10-17 15:03:51 +0200

    Starting new project - lecture 42.

---
+++ workspace/42_PointerArithmetic/src/PointerArithmetic.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : PointerArithmetic.cpp
+// Author      : Thomas Williams
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
