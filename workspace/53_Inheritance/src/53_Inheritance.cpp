7bae2e55f4c7f4f16a39b0aeaf46a1c885fe9479
Thomas Williams <thomas.williams@live.co.uk>
2016-10-18 13:04:03 +0200

    Writing code for lecture 53 myself.

--- workspace/53_Inheritance/src/53_Inheritance.cpp
+++ workspace/53_Inheritance/src/53_Inheritance.cpp
@@ -9,8 +9,33 @@
 #include <iostream>
 using namespace std;
 
+class Animal {
+public:
+	void speak() { cout << "Grrrrr." << endl; } ;
+};
+
+class Cat: public Animal {
+public:
+	void jump() { cout << "Cat jumping." << endl; } ;
+};
+
+class Tiger: public Cat {
+public:
+	void attackAntelope() { cout << "Tiger attacking antelope." << endl; } ;
+};
+
 int main() {
 
+	Animal a;
+	a.speak();
 
+	Cat c;
+	c.speak();
+	c.jump();
+
+	Tiger t;
+	t.speak();
+	t.jump();
+	t.attackAntelope();
 	return 0;
 }
