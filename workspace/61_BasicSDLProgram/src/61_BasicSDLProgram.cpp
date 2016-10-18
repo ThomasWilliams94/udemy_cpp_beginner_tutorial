66880d375ae42066388248eb21904687d25f43b7
Thomas Williams <thomas.williams@live.co.uk>
2016-10-18 18:26:43 +0200

    Writing out code for lecture 61 myself. Basic SDL program set up and running (including the properties needed to be set).

--- workspace/61_BasicSDLProgram/src/61_BasicSDLProgram.cpp
+++ workspace/61_BasicSDLProgram/src/61_BasicSDLProgram.cpp
@@ -7,9 +7,19 @@
 //============================================================================
 
 #include <iostream>
+#include <SDL.h>
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int main(int argc, char* argv[]) {
+
+	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+		cout << "SDL Init failed." << endl;
+		return 1;
+	}
+
+	cout << "SDL Init succeeded." << endl;
+
+	SDL_Quit();
+
 	return 0;
 }
