f953d610cd1e9916abf6f6c75df1a535a1442b63
Thomas Williams <thomas.williams@live.co.uk>
2016-10-18 15:15:28 +0200

    Writing out code for lecture 57 myself.

--- workspace/57_StaticKeyword/src/57_StaticKeyword.cpp
+++ workspace/57_StaticKeyword/src/57_StaticKeyword.cpp
@@ -9,7 +9,56 @@
 #include <iostream>
 using namespace std;
 
+// .h header file
+class Test {
+public:
+	// Initialisation of const must be done here
+	static int const MAX = 99;
+
+private:
+	int id;
+	static int count;
+
+public:
+	Test() {
+		id = count++;
+	}
+	;
+
+	int getId() {
+		return id;
+	}
+
+	static void showInfo() {
+		cout << count << endl;
+	}
+	;
+
+};
+
+// .cpp source
+int Test::count = 0;
+
 int main() {
+
+	cout << Test::MAX << endl;
+
+	// no Test obejcts yet created, so count == 0
+	Test::showInfo();
+
+	// initialiser increments count variable
+	Test test1;
+	cout << "Object 1 id: " << test1.getId() << endl;
+
+	Test::showInfo();
+
+	Test test2;
+	cout << "Object 2 id: " <<test2.getId() << endl;
+	Test::showInfo();
+
+	Test test3;
+	cout << "Object 3 id: " << test3.getId() << endl;
+	Test::showInfo();
 
 	return 0;
 }
