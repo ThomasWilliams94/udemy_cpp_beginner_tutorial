ee9867b9c9a99a59f31d36c12fafd488976c8428
Thomas Williams <thomas.williams@live.co.uk>
2016-10-18 13:27:24 +0200

    Writing out code for lecture 55 myself.

--- workspace/55_ConstructorInheritance/src/55_ConstructorInheritance.cpp
+++ workspace/55_ConstructorInheritance/src/55_ConstructorInheritance.cpp
@@ -9,7 +9,54 @@
 #include <iostream>
 using namespace std;
 
+class Machine {
+private:
+	int id;
+public:
+	Machine() :
+			id(42) {
+		cout << "Machine no-argument constructor called." << endl;
+	}
+	;
+	Machine(int id) :
+			id(id) {
+		cout << "Machine parameterised constructor called." << endl;
+	}
+	;
+
+	void info() {
+		cout << "ID: " << id << endl;
+	}
+	;
+};
+
+class Vehicle: public Machine {
+public:
+	Vehicle() {
+		cout << "Vehicle no-argument constructor called." << endl;
+	}
+	;
+	Vehicle(int id) :
+			Machine(id) {
+		cout << "Vehicle parameterised constructor called." << endl;
+	}
+	;
+
+};
+
+class Car: public Vehicle {
+public:
+	Car(): Vehicle(999) {
+		cout << "Car no-argument constructor called." << endl;
+	}
+	;
+
+};
+
 int main() {
+
+	Car car;
+	car.info();
 
 	return 0;
 }
