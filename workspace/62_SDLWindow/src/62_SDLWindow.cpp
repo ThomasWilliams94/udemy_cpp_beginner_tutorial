f46574309056ff7b1eea3b5e009472a45473c099
Thomas Williams <thomas.williams@live.co.uk>
2016-10-18 19:01:49 +0200

    Writing code for lecture 62 myself. SDL window created, then closed when 'x' is clicked.

--- workspace/62_SDLWindow/src/62_SDLWindow.cpp
+++ workspace/62_SDLWindow/src/62_SDLWindow.cpp
@@ -11,15 +11,45 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+	const int SCREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
 
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
 		cout << "SDL Init failed." << endl;
 		return 1;
 	}
 
-	cout << "SDL Init succeeded." << endl;
+	SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion",
+			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+			SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
+	if(window == NULL) {
+		SDL_Quit();
+		cout << "Could not create window: " << SDL_GetError() << endl;
+		return 2;
+	}
+
+	bool quit = false;
+
+	SDL_Event event;
+
+	while(!quit) {
+		// Update particles
+		// Draw particles
+		// Check for messages/events
+
+
+		while(SDL_PollEvent(&event)) {
+			if(event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
 	SDL_Quit();
+
+	cout << "Quitting..." << endl;
 
 	return 0;
 }
