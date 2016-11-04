26de19f428bac264c7b91547a9711a2d925e0381
Thomas Williams <thomas.williams@live.co.uk>
2016-11-04 21:15:20 +0100

    Completing course.

--- workspace/SDL_Project/src/Particle.cpp
+++ workspace/SDL_Project/src/Particle.cpp
@@ -11,18 +11,37 @@
 
 namespace tjw {
 
-Particle::Particle() : m_x(0), m_y(0) {
-	m_direction = (2.0 * M_PI * rand())/RAND_MAX;
-	m_speed = ((2.0 * rand()/RAND_MAX) - 1) * 0.0002;
+Particle::Particle() :	m_x(0), m_y(0) {
+	init();
+}
+
+void Particle::init() {
+	m_x = 0;
+	m_y = 0;
+	m_direction = (2.0 * M_PI * rand()) / RAND_MAX;
+	m_speed = ((2.0 * rand() / RAND_MAX) - 1) * 0.04;
+
+	m_speed *= m_speed;
 }
 
 void Particle::update(int interval) {
+
+	m_direction += interval * 0.0015;
+
 	double xSpeed = m_speed * cos(m_direction);
 	double ySpeed = m_speed * sin(m_direction);
 
 	// Interval is similar (the same as?) to deltaTime in Unity?
 	m_x += xSpeed * interval;
 	m_y += ySpeed * interval;
+
+	if (m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1) {
+		init();
+	}
+
+	if(rand() < RAND_MAX / 100) {
+		init();
+	}
 }
 
 Particle::~Particle() {
--- workspace/SDL_Project/src/Particle.h
+++ workspace/SDL_Project/src/Particle.h
@@ -17,12 +17,17 @@ struct Particle {
 	double m_x;
 	double m_y;
 
+private:
 	double m_speed;
 	double m_direction;
+
 public:
 	Particle();
 	virtual ~Particle();
 	void update(int interval);
+
+private:
+	void init();
 };
 
 } /* namespace tjw */
